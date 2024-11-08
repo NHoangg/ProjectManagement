7e03f99085455b52afaea1c91a113e1667c9a45e
NHoangg <156037158+NHoangg@users.noreply.github.com>
2024-11-08 20:27:09 +0700

    Update ProjectManger.cs

--- DOANOOPCK/ProjectManger.cs
+++ DOANOOPCK/ProjectManger.cs
@@ -1,8 +1,15 @@
-﻿[Serializable]
+using System.Threading.Tasks;
+
+[Serializable]
 public class ProjectManager
 {
     private List<Project> projects = new List<Project>();
-
+    private List<TaskBase> tasks = new List<TaskBase>();
+    public List<TaskBase> Tasks { get { return tasks; } }
+    public void AddTask(TaskBase task)
+    {
+        tasks.Add(task);
+    }
     public void AddProject(Project project)
     {
         projects.Add(project);
@@ -15,19 +22,17 @@ public class ProjectManager
             Console.WriteLine("Project: " + project.ProjectName + ", Start: " + project.StartDate + ", End: " + project.EndDate);
         }
     }
-
+    public void ListTasks()
+    {
+        foreach (TaskBase task in tasks)
+        {
+            Console.WriteLine("Task: " + task.Name + ", Due: " + task.DueDate);
+        }
+    }
     public Project GetProjectByName(string name)
     {
         return projects.Find(p => p.ProjectName.Equals(name, StringComparison.OrdinalIgnoreCase));
     }
-        public List<TaskBase> Tasks { get; set; } = new List<TaskBase>();
-
-        public void ListTasks()
-        {
-            foreach (var task in Tasks)
-            {
-                Console.WriteLine($"Task: {task.Name}, Due: {task.DueDate}");
-            }
-        }
+        public List<TaskBase> TaskBase { get; set; } = new List<TaskBase>();
     }
 
